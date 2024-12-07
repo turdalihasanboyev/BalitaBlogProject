b075f0762536e439e1dd9f1fb2d45b2f581dbbea
turdal1hasanboyev <turdalihasanboyev70.com>
2024-12-07 09:46:29 +0500

    finish

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import CategoryPageView
 
 
 urlpatterns = [
-    
+    path('category/<slug:slug>/', CategoryPageView.as_view(), name='category'),
 ]
--- apps/category/views.py
+++ apps/category/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Category
+
+
+class CategoryPageView(View):
+    template_name = 'category.html'
+
+    def get(self, request, *args, **kwargs):
+        slug = kwargs.get('slug')
+        category = get_object_or_404(Category, slug__iexact=slug)
+        articles = category.articles_category.all()
+        context = {
+            'category': category,
+            'articles': articles,
+        }
+        return render(request, self.template_name, context)
