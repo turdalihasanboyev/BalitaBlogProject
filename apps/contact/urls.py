bc964a6559ec74a9a9b8102d21d13232977d3e07
turdal1hasanboyev <turdalihasanboyev70.com>
2024-12-07 09:22:05 +0500

    Updating codes

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import ContactPageView
 
 
 urlpatterns = [
-    
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Contact
+
+
+class ContactPageView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'contact.html')
+    def post(self, request, *args, **kwargs):
+        url = request.build_absolute_uri()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, phone_number=phone_number, message=message)
+        contact.save()
+        return redirect(url)
