a0ee1151c10797ae26f6eca36bf198ebb4e30fd3
Turdali <turdalihasanboyev144@gmail.com>
2025-05-26 07:42:38 +0500

    docker

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'balita_db',
         'USER': 'balita_db',
         'PASSWORD': 'balita_db',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
